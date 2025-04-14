3005a276782a1eb0201d7cda893cdd789a2fb543
lijin <lijin@cavemanstudio.net>
2025-04-14 13:16:25 +0800

    init

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+# 读取requirements.txt自动生成install_requires
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(name='python-nakama',
       version='1.0.0',
       description='Python client for Nakama server',
       packages=find_packages(),
+      install_requires=install_requires,
       license='MIT',
       author='tudou',
       author_email='244395692@qq.com',
